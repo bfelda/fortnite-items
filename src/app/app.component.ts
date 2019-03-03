ad05683741b19faf37d9b7bdaee060314cec0f61
bfelda <bfelda@gmail.com>
2019-03-03 15:27:04 -0600

    added redirect file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'overwatch-app';
+
+  private heros: any[] = [];
+
+  constructor(private http:HttpClient) {
+    this.GetOverwatchHeros().toPromise().then((heros => this.heros = heros));
+  }
+
+  title = 'Fortnite items';
+
+  GetOverwatchHeros() {
+    return this.http.get('https://fortnite-public-api.theapinetwork.com/prod09/items/list');
+  }
 }
